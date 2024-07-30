833f9b26003ec014d9430b7e7677b7459b0ec67d
JosepharDev <josephardev@gmail.com>
2024-07-30 10:49:34 -0700

    update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" returns information about his/her TODO list progress """
-
+"""Python script that, using fake rest api, for a given employee ID"""
 import requests
 import sys
 
@@ -8,7 +7,8 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    completed = [s.get("title") for s in todos if s.get("completed") is True]
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
