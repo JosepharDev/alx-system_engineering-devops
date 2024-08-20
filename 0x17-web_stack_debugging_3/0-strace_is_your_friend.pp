3b4451767fc0ffde19a39b687ff766167f9aa7b9
JosepharDev <josephardev@gmail.com>
2024-08-20 16:15:06 -0700

    fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix extantion error of wp-settings.php instead of wp-setting.phpp
-{ 'fix typo':
+exec { 'fix typo':
   onlyif  => 'test -e /var/www/html/wp-settings.php',
   command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
