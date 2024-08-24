afa3bab7f9203686e3fa747d63756895549f74a5
JosepharDev <josephardev@gmail.com>
2024-08-24 05:07:53 -0700

    add queries reddit api task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""module documentation task 0"""
+import request
+
+
+def number_of_subscribers(subreddit):
+    """queries the reddit api and return the number of
+    subscribers (not active users, total subscribers)
+    on a given subreddit"""
+
+    response = requests.get("https://www.reddit.com/r/{}/about.json"
+                            .format(subreddit),
+                            headers={"User-Agent": "test-User-Agent"},
+                            allow_redirects=False)
+    if response.status_code >= 300:
+        return 0
+
+    return response.json().get("data").get("subscribers")
